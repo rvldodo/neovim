4493cb7f6e3621e621ec306cbeecc40b2610c76b
Rivaldo Lawalata <nono.qwerty28@gmail.com>
2024-01-07 03:49:57 +0700

    colorscheme and go settings

--- lua/dodo/lazy.lua
+++ lua/dodo/lazy.lua
@@ -11,9 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{import = "dodo.plugins"}, {import = "dodo.plugins.lsp"}}, {
+require("lazy").setup({ { import = "dodo.plugins" }, { import = "dodo.plugins.lsp" } }, {
   install = {
-    colorscheme = { "nightfly" },
+    colorscheme = { "vscode" },
   },
   checker = {
     enabled = true,
--- lua/dodo/plugins/colorscheme.lua
+++ lua/dodo/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
   {
-    "bluz71/vim-nightfly-guicolors",
+    "Mofiqul/vscode.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
+      vim.cmd([[colorscheme vscode]])
     end,
   },
 }
